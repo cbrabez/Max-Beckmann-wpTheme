09e80caa876d83833eb927f43918eedba28f4d7e
cbrabez <christian.brabez@googlemail.com>
2012-10-19 09:22:06 +0200

    category pages updated

--- category-klasse2b.php
+++ category-klasse2b.php
@@ -11,26 +11,20 @@
 		<h1 class="ueberschriftContent">Vergangene Beitr&auml;ge der Klasse 2b</h1>
 	
 		<div class="posts-by-category">
-
-		<?php if (!is_paged() and is_home()) {
-		      query_posts('cat=4'); 
-		      wp_reset_query();
-		} ?>
-
-		<ol class="rectangle-list">
-			<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-				<li><h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2></li>			
-			<?php endwhile; ?>
-		</ol>
-
-		<?php else : ?>
-
-	<h2>Nothing FOUND</h2>
-	<p>You are looking for something that isn't here!?!</p>
-	<p><a href="<?php echo get_option('home'); ?>">Return to the Homepage</a></p>
-
-<?php endif; ?>
-
+			<?php if(have_posts()) :?>
+				<ol class="rectangle-list">
+					<?php while(have_posts()) : the_post(); ?>
+						<li><h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2></li>			
+					<?php endwhile; ?>
+				</ol>
+	
+			<?php else : ?>
+			
+				<h2>Nothing FOUND</h2>
+				<p>You are looking for something that isn't here!?!</p>
+				<p><a id="link-to-index" href="<?php echo get_option('home'); ?>">Return to the Homepage</a></p>
+			
+			<?php endif; ?>
 		</div>
 	</section>
 	<!-- end content -->
