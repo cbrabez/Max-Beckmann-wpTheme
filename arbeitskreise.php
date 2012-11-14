cf913f20e0088e0d0c1953a3efc090544399b249
cbrabez <christian.brabez@googlemail.com>
2012-11-14 15:42:09 +0100

    update beim arbeitskreise layout

--- arbeitskreise.php
+++ arbeitskreise.php
@@ -69,7 +69,7 @@
 <div class="hidden-content" id="arbeitskreis_chor-content">
   <div class="details">
     <h4>Arbeitskreis 1</h4>
-    <img src="http://www.placehold.it/150x200" />
+    <img src="http://www.placehold.it/150x200" class="float-left" />
     <p>
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
     </p>
@@ -82,12 +82,18 @@
 <div class="hidden-content" id="arbeitskreis_computer-content">
   <div class="details">
     <h4>Arbeitskreis 2</h4>
-    <img src="http://www.placehold.it/150x200" />
+    
     <p>
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+      <img src="http://www.placehold.it/150x200" class="float-left" />
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
     </p>
+
+   <p>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.
+    </p>
+<img src="http://www.placehold.it/150x200" class="float-right" />
     <p>
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata 
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.
     </p>
   </div>
 </div>
@@ -95,7 +101,7 @@
 <div class="hidden-content" id="arbeitskreis_gartengruppe-content">
   <div class="details">
     <h4>Arbeitskreis 3</h4>
-    <img src="http://www.placehold.it/150x200" />
+    <img src="http://www.placehold.it/150x200" class="float-left" />
     <p>
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
     </p>
@@ -108,7 +114,7 @@
 <div class="hidden-content" id="arbeitskreis_kochen-content">
   <div class="details">
     <h4>Arbeitskreis 4</h4>
-    <img src="http://www.placehold.it/150x200" />
+    <img src="http://www.placehold.it/150x200" class="float-left" />
     <p>
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
     </p>
@@ -121,7 +127,7 @@
 <div class="hidden-content" id="arbeitskreis_orff-content">
   <div class="details">
     <h4>Arbeitskreis 5</h4>
-    <img src="http://www.placehold.it/150x200" />
+    <img src="http://www.placehold.it/150x200" class="float-left" />
     <p>
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
     </p>
@@ -134,7 +140,7 @@
 <div class="hidden-content" id="arbeitskreis_streitschlichter-content">
   <div class="details">
     <h4>Arbeitskreis 6</h4>
-    <img src="http://www.placehold.it/150x200" />
+    <img src="http://www.placehold.it/150x200" class="float-left" />
     <p>
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
     </p>
@@ -147,7 +153,7 @@
 <div class="hidden-content" id="arbeitskreis_sport-content">
   <div class="details">
     <h4>Arbeitskreis 7</h4>
-    <img src="http://www.placehold.it/150x200" />
+    <img src="http://www.placehold.it/150x200" class="float-left" />
     <p>
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
     </p>
@@ -160,7 +166,7 @@
 <div class="hidden-content" id="arbeitskreis_streitschlichter-content">
   <div class="details">
     <h4>Arbeitskreis 8</h4>
-    <img src="http://www.placehold.it/150x200" />
+    <img src="http://www.placehold.it/150x200" class="float-left" />
     <p>
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
     </p>
