7ff29a67d5ce02660aceaeeedebb1e47b6e87a6b
Christian Brabez <cbrabez@christians-imac.fritz.box>
2013-03-14 07:13:24 +0100

    Update Chronik

---
+++ category-ehemalige.php
@@ -0,0 +1,42 @@
+
+<?php get_header(); ?>
+
+<!-- start outerWrapper -->
+	<section id="outerWrapper" class="background-aktuelles">
+	<!-- start innerWrapper -->
+	<section id="innerWrapper">
+	
+	<!-- start content -->
+	<section id="category" class="content">
+		<h1>Beitr&auml;ge vergangenener Jahrg&auml;nge</h1>
+	
+	<div class="posts-by-category">
+			<?php if(have_posts()) :?>
+				<ol class="rectangle-list">
+					<?php while(have_posts()) : the_post(); ?>
+						<li><h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2></li>			
+					<?php endwhile; ?>
+				</ol>
+	
+			<?php else : ?>
+			
+				<h2>Leider haben wir keine Beitr&auml;ge gefunden.</h2>
+				<p>Im <a href="<?php bloginfo('url');?>/archiv">Archiv</a> haben Sie einen Überblick aller vergangener Artikel.</p>
+				<p>Im Bereich <a id="link-to-index" href="<?php echo get_option('home'); ?>">Aktuelles</a> finden Sie die letzten Beitr&auml;ge.</p>
+			
+			<?php endif; ?>
+		</div>
+	</section>
+	<!-- end content -->
+
+	
+
+<?php get_sidebar(); ?>
+
+	</section>
+	<!-- end innerWrapper -->
+	
+	</section>
+	<!-- end outerWrapper -->
+
+<?php get_footer(); ?>
---
+++ wochenplaene.php
@@ -0,0 +1,54 @@
+<?php /* Template Name:Wochenplaene */ ?>
+
+<?php get_header(); ?>
+
+			<!-- start outerWrapper -->
+	<section id="outerWrapper" class="background-aktuelles">
+	<!-- start innerWrapper -->
+	<section id="innerWrapper">
+	
+	<!-- start content -->
+	<section id="aktuelles" class="content">
+		<h1>&Uuml;bersicht der Wochenpl&auml;ne</h1>
+	
+	<?php if (have_posts()) : ?>
+		<?php while (have_posts()) : the_post(); ?>
+	
+		<article <?php post_class() ?>>
+			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+			
+			<span class="date">
+				<?php the_time('j') ?><br />
+				<span><?php the_time('M') ?></span>
+			</span>
+			<span class="year"><?php the_time('Y') ?></span>
+			</h2>
+			
+			<p><?php the_content(''); ?> </p>
+		</article>
+		
+	<?php endwhile; ?>
+
+
+<?php else : ?>
+
+	<h2>Nothing FOUND</h2>
+	<p>You are looking for something that isn't here!?!</p>
+	<p><a href="<?php echo get_option('home'); ?>">Return to the Homepage</a></p>
+
+<?php endif; ?>
+
+	</section>
+	<!-- end content -->
+
+	<!-- start sidebar -->
+
+<?php get_sidebar(); ?>
+
+	</section>
+	<!-- end innerWrapper -->
+	
+	</section>
+	<!-- end outerWrapper -->
+
+<?php get_footer(); ?>
